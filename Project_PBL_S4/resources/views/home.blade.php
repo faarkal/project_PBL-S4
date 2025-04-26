5e930a46ab34f1448776b540a8825c167ebd860d
faarkal <farukalfatah03@gmail.com>
2025-04-26 09:31:28 +0700

    comit

--- Project_PBL_S4/resources/views/home.blade.php
+++ Project_PBL_S4/resources/views/home.blade.php
@@ -3,11 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laporan Produksi - Balai Usaha Perikanan Genteng</title>
+    <title>Balai Usaha Perikanan Genteng</title>
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-
 </head>
 <body>
     <header>
@@ -32,20 +30,15 @@
                 </div>
                 <a href="{{ route('home') }}">HOME</a>
             </li>
-            <li class="dropdown">
-                <a href="#" class="dropbtn" onclick="toggleDropdown()">PROFILE</a>
-                <div id="dropdownMenu" class="dropdown-content">
-                    <a href="#">Biodata Kepala Dinas</a>
-                    <a href="#">Data Profile Dinas</a>
-                    <a href="#">Visi-Misi</a>
-                </div>
+            <li>
+                <a href="#">PROFILE</a>
             </li>
             <li class="dropdown">
                 <a href="#" class="dropbtn" onclick="toggleDropdown('laporanMenu')">LAPORAN</a>
                 <div id="laporanMenu" class="dropdown-content">
                     <a href="{{ route('laporan.produksi') }}">Laporan Produksi</a>
                     <a href="#">Laporan Penjualan</a>
-                    <a href="{{ route('hasil.laporan.induk') }}">Laporan Induk</a>
+                    <a href="#">Laporan Induk</a>
                 </div>
             </li>
             <li class="dropdown">
@@ -57,12 +50,13 @@
                 </div>
             </li>
 
-            <li><a href="/nota">NOTA</a></li>
+            <li><a href="#">NOTA</a></li>
         </ul>
     </nav>
 
+
     <main>
-    <section class="main-content">
+        <section class="main-content">
             <div class="info-kiri-text">
                 <h2>Selamat Datang di Balai Usaha Perikanan Genteng</h2>
                 <br>
@@ -80,71 +74,68 @@
                     <div class="foto-container">
                         <div class="foto-placeholder">FOTO</div>
                     </div>
-                    <p>Nama Kepala Dinas<br>Balai Usaha Perikanan Genteng</p>
+                    <p>Nama Menteri<br>Menteri Kelautan dan Perikanan</p>
             </div>
-
         </section>
     </main>
 
-    <section class="info-perikanan">
-        <div class="info-kiri">
-            <div class="perikanan-header">
-                <div class="logo">
-                    <img src="{{ asset('images/kementrian_perikanan.png') }}" alt="Logo Ikan">
+        <section class="info-perikanan">
+            <div class="info-kiri">
+                <div class="perikanan-header">
+                    <div class="logo">
+                        <img src="{{ asset('images/kementrian_perikanan.png') }}" alt="Logo Ikan">
+                    </div>
+                    <h2>PERIKANAN</h2>
                 </div>
-                <h2>PERIKANAN</h2>
+                    <p><strong>DINAS PERIKANAN BANYUWANGI</strong></p>
+                    <p>UP: Balai Benih Ikan Genteng<br>Jl. KH. Agus Salim No.106, Lingkungan Cuking RW, Mojopanggung, Kec. Banyuwangi, Kabupaten Banyuwangi, Jawa Timur 68424.</p>
+                    <p>Telephone:</p>
+                    <p>Email:</p>
+                <div class="social-icons">
+                    <a href="#" style="color: #c32aa3;"><i class="fab fa-instagram"></i></a>
+                    <a href="#" style="color: #ff0000;"><i class="fab fa-youtube"></i></a>
+                    <a href="#" style="color: #1da1f2;"><i class="fab fa-twitter"></i></a>
+                    <a href="#" style="color: #3b5998;"><i class="fab fa-facebook"></i></a>
+                </div>
             </div>
-                <p><strong>DINAS PERIKANAN BANYUWANGI</strong></p>
-                <p>UP: Balai Benih Ikan Genteng<br>Jl. KH. Agus Salim No.106, Lingkungan Cuking RW, Mojopanggung, Kec. Banyuwangi, Kabupaten Banyuwangi, Jawa Timur 68424.</p>
-                <p>Telephone:</p>
-                <p>Email:</p>
-            <div class="social-icons">
-                <a href="#" style="color: #c32aa3;"><i class="fab fa-instagram"></i></a>
-                <a href="#" style="color: #ff0000;"><i class="fab fa-youtube"></i></a>
-                <a href="#" style="color: #1da1f2;"><i class="fab fa-twitter"></i></a>
-                <a href="#" style="color: #3b5998;"><i class="fab fa-facebook"></i></a>
+
+            <div class="info-kanan">
+                <h3>HARI KUNJUNGAN</h3>
+                <ul>
+                    <li>Senin: 07:00-17:00</li>
+                    <li>Selasa: 07:00-17:00</li>
+                    <li>Rabu: 07:00-17:00</li>
+                    <li>Kamis: 07:00-17:00</li>
+                    <li>Jumat: Tutup</li>
+                    <li>Sabtu: Tutup</li>
+                    <li>Minggu: 07:00-17:00</li>
+                </ul>
+            </div>
+        </section>
+
+        <div id="popup" class="popup">
+            <div class="popup-content">
+                <span class="close" onclick="closePopup()">&times;</span>
+                <h3>Pilih Laporan</h3>
+                <ul>
+                    <li><a href="#">Laporan Produksi</a></li>
+                    <li><a href="#">Laporan Penjualan</a></li>
+                    <li><a href="#">Laporan Induk</a></li>
+                </ul>
             </div>
         </div>
-
-        <div class="info-kanan">
-            <h3>HARI KUNJUNGAN</h3>
-            <ul>
-                <li>Senin: 07:00-17:00</li>
-                <li>Selasa: 07:00-17:00</li>
-                <li>Rabu: 07:00-17:00</li>
-                <li>Kamis: 07:00-17:00</li>
-                <li>Jumat: Tutup</li>
-                <li>Sabtu: Tutup</li>
-                <li>Minggu: 07:00-17:00</li>
-            </ul>
-        </div>
-    </section>
-
-    <div id="popup" class="popup">
-        <div class="popup-content">
-            <span class="close" onclick="closePopup()">&times;</span>
-            <h3>Pilih Laporan</h3>
-            <ul>
-                <li><a href="#">Laporan Produksi</a></li>
-                <li><a href="#">Laporan Penjualan</a></li>
-                <li><a href="{{ route('hasil.laporan.induk') }}">Laporan Induk</a></li>
-            </ul>
-        </div>
-    </div>
-    <br>
-
-
     <footer>
         <p style="display: flex; justify-content: center; align-items: center; font-size: 0.8em;">
             Copyright © 2025 Perikanan Kab. Banyuwangi - All rights reserved. | Create by PBL kelompok1.
         </p>
     </footer>
 
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
     <script>
         function toggleHamburgerMenu() {
             const menuContent = document.getElementById("hamburgerMenuContent");
-
-
+            
             if (menuContent.style.display === "block") {
                 menuContent.style.display = "none";
             } else {
@@ -152,10 +143,9 @@
             }
         }
 
-
         window.onclick = function(event) {
             const menuContent = document.getElementById("hamburgerMenuContent");
-
+            
             if (!event.target.closest("nav ul li a[onclick='toggleHamburgerMenu()']")) {
                 menuContent.style.display = "none";
             }
@@ -163,69 +153,28 @@
     </script>
 
     <script>
-
-        document.querySelector('.dropbtn').addEventListener('click', function (event) {
-            event.preventDefault(); // Mencegah default link behavior
-            const dropdown = document.querySelector('.dropdown-content');
-
-            // Toggle tampilan dropdown
-            if (dropdown.style.display === 'block') {
-                dropdown.style.display = 'none';
-            } else {
-                dropdown.style.display = 'block';
-            }
-        });
-
-        // Tutup dropdown jika klik di luar
-        window.addEventListener('click', function (event) {
-            const dropdown = document.querySelector('.dropdown-content');
-
-            if (!event.target.matches('.dropbtn')) {
-                dropdown.style.display = 'none';
-            }
-        });
-    </script>
-
-    <script>
-        function toggleHamburgerMenu() {
-            const menuContent = document.getElementById("hamburgerMenuContent");
-
-            // Toggle tampilan menu tambahan
-            if (menuContent.style.display === "block") {
-                menuContent.style.display = "none";
-
         function toggleDropdown(menuId) {
             const menu = document.getElementById(menuId);
-
 
             if (menu.style.display === "block") {
                 menu.style.display = "none";
             } else {
-
                 const allDropdowns = document.querySelectorAll('.dropdown-content');
                 allDropdowns.forEach(function(dropdown) {
-                    dropdown.style.display = "none";
+                    dropdown.style.display = "none"; 
                 });
-                menu.style.display = "block";
+                menu.style.display = "block"; 
             }
         }
 
         window.onclick = function(event) {
-
-            const menuContent = document.getElementById("hamburgerMenuContent");
-
-            if (!event.target.closest("nav ul li a[onclick='toggleHamburgerMenu()']")) {
-                menuContent.style.display = "none";
-            }
-
             const dropdowns = document.querySelectorAll('.dropdown-content');
-
+            
             dropdowns.forEach(function(dropdown) {
                 if (!event.target.closest('.dropbtn')) {
-                    dropdown.style.display = "none";
+                    dropdown.style.display = "none"; 
                 }
             });
-
         }
 
         document.querySelectorAll('.dropdown-content a').forEach(function(item) {
